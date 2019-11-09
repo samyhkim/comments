8737ee8abdc84dc4a3eeebe3829ecc03de6fa726
Samuel Kim <samuelkim@Samuels-MacBook-Pro.local>
2019-11-08 18:06:45 -0800

    created component CommentDetails and set up props: author, timeAgo, & avatar

--- src/index.js
+++ src/index.js
@@ -1,23 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import faker from "faker";
+import CommentDetails from "./CommentDetails";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" />
-        </a>
-        <div className="content">
-          <a href="/" className="author">
-            Sam
-          </a>
-          <div className="metadata">
-            <span className="date">Today at 6:00PM</span>
-          </div>
-          <div className="text">Nice blog post!</div>
-        </div>
-      </div>
+      <CommentDetails
+        author="Sam"
+        timeAgo="Today at 4:45PM"
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetails
+        author="Alex"
+        timeAgo="Today at 2:00AM"
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetails
+        author="Jane"
+        timeAgo="Yesterday at 5:00PM"
+        avatar={faker.image.avatar()}
+      />
     </div>
   );
 };
