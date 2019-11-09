732f197bc47564403b49b6d1c5c1ad0ddf98b2ed
Samuel Kim <samuelkim@Samuels-MacBook-Pro.local>
2019-11-08 19:00:29 -0800

    nested CommentDetails component within ApprovalCard using props.children

--- src/index.js
+++ src/index.js
@@ -2,16 +2,25 @@ import React from "react";
 import ReactDOM from "react-dom";
 import faker from "faker";
 import CommentDetails from "./CommentDetails";
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetails
-        author="Sam"
-        timeAgo="Today at 4:45PM"
-        avatar={faker.image.avatar()}
-        content="Great blog post!"
-      />
+      <ApprovalCard>
+        <div>
+          <h4>Warning!</h4>
+          Are you sure you want to do this?
+        </div>
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetails
+          author="Sam"
+          timeAgo="Today at 4:45PM"
+          avatar={faker.image.avatar()}
+          content="Great blog post!"
+        />
+      </ApprovalCard>
       <CommentDetails
         author="Alex"
         timeAgo="Today at 2:00AM"
